7053df0d0e83f864e5cf74d24ef7e5cac2af426a
ana vakhtangishvili <vakhtangishviliana@gmail.com>
2024-01-17 10:27:03 +0400

    create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { DataService } from './services/data.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  providers: [DataService],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    BarChartComponent,
+    PieChartComponent,
+    LineChartComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'D3-Charts';
+export class AppComponent implements OnInit {
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getParsedData('assets/population.csv').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
---
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'app-bar-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './bar-chart.component.html',
+  styleUrl: './bar-chart.component.scss',
+})
+export class BarChartComponent implements OnInit {
+ ngOnInit(): void {
+     
+ } 
+}
